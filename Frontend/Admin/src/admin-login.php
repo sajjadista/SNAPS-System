ba081f9c54c12746aacb3d9bf4a04c5da4ed3498
Sajjad <71213151+sajjadista@users.noreply.github.com>
2022-06-08 16:30:19 +0800

    Created and styled the admin login page

--- Frontend/Admin/src/admin-login.php
+++ Frontend/Admin/src/admin-login.php
@@ -6,15 +6,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SNAPS Admin Portal Login</title>
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
 
 </head>
 
 <body>
 
+    <div class="container panel center-screen py-5 px-3">
 
+        <div class="row my-3">
+            <div class="col">
+                <img src="" alt="">
+            </div>
+        </div>
 
+        <div class="row my-3">
+            <div class="col d-flex justify-content-center">
+                <h2>Admin Login</h2>
+            </div>
+        </div>
+
+        <form action="">
+            <div class="row my-3">
+                <div class="col d-flex justify-content-center">
+                    <input class="textfield-theme" type="text" name="username" id="username" placeholder="Admin Username">
+                </div>
+            </div>
+
+            <div class="row my-3">
+                <div class="col d-flex justify-content-center">
+                    <input class="textfield-theme" type="text" name="admin-id" id="admin-id" placeholder="Admin ID">
+                </div>
+            </div>
+
+            <div class="row my-3">
+                <div class="col d-flex justify-content-center">
+                    <input class="textfield-theme" type="password" name="password" id="password" placeholder="Password">
+                </div>
+            </div>
+
+            <div class="row my-3 ">
+                <div class="col d-flex justify-content-center">
+                    <input class=" button-theme" type="submit" value="Login">
+                </div>
+            </div>
+        </form>
+
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
