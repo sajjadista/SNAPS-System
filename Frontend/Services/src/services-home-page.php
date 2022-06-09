34d8e6707de5aaba0caffc122beda4f9a34b027b
Harvin Raj <harvinraj034@gmail.com>
2022-06-10 02:10:53 +0800

    item 1 Bukit Ekspo

--- Frontend/Services/src/services-home-page.php
+++ Frontend/Services/src/services-home-page.php
@@ -5,14 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
+    <title>SNAPS Service Home Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
 </head>
 
 <body>
- <p> Hello, My name is Harvin. Welcome <p>
+  <div class="grid-container">
+    <div class="item1">
+      <img id="bukit-ekspo" class="image-fluid" src="../assets/bukit ekspo.JPG" alt="Bukit Ekspo">
+      <div class="left">BUKIT EKSPO</div>
+    </div>
+    <div class="item2">MACHINERIES</div>
+    <div class="item3">MEETING AND LECTURE ROOMS</div>
+    <div class="item4">AGRICULTURAL COURSES</div>
+  </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
