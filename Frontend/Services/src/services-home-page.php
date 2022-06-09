1ecf4e79ae75883afac060860fc3a883b33defeb
Harvin Raj <harvinraj034@gmail.com>
2022-06-10 02:16:21 +0800

    Item 2 machineries

--- Frontend/Services/src/services-home-page.php
+++ Frontend/Services/src/services-home-page.php
@@ -17,7 +17,10 @@
       <img id="bukit-ekspo" class="image-fluid" src="../assets/bukit ekspo.JPG" alt="Bukit Ekspo">
       <div class="left">BUKIT EKSPO</div>
     </div>
-    <div class="item2">MACHINERIES</div>
+    <div class="item2">
+      <img id="machineries" class="image-fluid" src="../assets/machineries.jpg" alt="Machineries">
+      <div class="left">MACHINERIES</div>
+    </div>
     <div class="item3">MEETING AND LECTURE ROOMS</div>
     <div class="item4">AGRICULTURAL COURSES</div>
   </div>
