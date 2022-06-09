4f7adf7cf1dc8c059e05a6457f5be35dea742497
Sajjad <71213151+sajjadista@users.noreply.github.com>
2022-06-09 10:08:16 +0800

    Created and styled the user details page

---
+++ Frontend/Admin/src/user-details.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SNAPS Admin Portal Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+
+    <div class="container panel center-screen py-5 px-3">
+
+        <div class="row my-3">
+            <div class="col d-flex justify-content-center">
+                <img id="user-photo" class="image-fluid" src="../assets/user-demo-picture.jpg" alt="User Photo">
+            </div>
+        </div>
+
+
+        <div class="row my-3">
+            <div class="col d-flex justify-content-center">
+                <h2>User details</h2>
+
+            </div>
+        </div>
+        <hr class="section-divider">
+
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">Username:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="The Best User Ever" readonly></div>
+        </div>
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">Phone number:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="+60 12 233 3444" readonly></div>
+        </div>
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">Gender:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="Female" readonly></div>
+        </div>
+
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">Account status:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="Enabled" readonly></div>
+        </div>
+
+        <div class="row my-4">
+
+            <div class="col mt-4 d-flex justify-content-center">
+                <h4>Address</h4>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col mb-4 d-flex justify-content-center">
+                <hr class="section-divider">
+            </div>
+
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">Unit Number:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="33" readonly></div>
+        </div>
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">Street:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="Jalan Imbi 1/4" readonly></div>
+        </div>
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">City:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="Kuala Lumpur" readonly></div>
+        </div>
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">State:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="W.P Kuala Lumpur" readonly></div>
+        </div>
+
+        <div class="row my-4">
+            <div class="col d-flex justify-content-center">
+                <h5 class="label-theme">Postal code:</h5>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col d-flex justify-content-center"><input class="textfield-theme" type="text" value="123456" readonly></div>
+        </div>
+
+
+
+        <div class="row my-5">
+            <div class="col d-flex justify-content-center">
+                <button class="button-theme block">Block</button>
+            </div>
+            <div class="col d-flex justify-content-center">
+                <button class="button-theme">Purchase History</button>
+            </div>
+        </div>
+
+
+
+
+
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
