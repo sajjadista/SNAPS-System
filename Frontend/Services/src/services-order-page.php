e9d98e2eaac2ba530cfedfc2afd9d8d6d60bed1d
Harvin Raj <harvinraj034@gmail.com>
2022-06-10 16:14:17 +0800

    Added Services Feedback Page

---
+++ Frontend/Services/src/services-order-page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SNAPS Service Order Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+  <div class="grid-container">
+    <div class="item1">
+      <div class="right">Bukit Ekspo booking for 18th April 2022</div>
+        <a href="services-feedback-page.php">
+          <button class="button button1">Feedback</button>
+        </a>
+        <a href="services-home-page.php">
+          <button class="button button2">Cancel</button>
+        </a>
+    </div>
+    <div class="item2">
+      <div class="right">Machineries booking for 20th May 2022</div>
+        <a href="services-feedback-page.php">
+          <button class="button button1">Feedback</button>
+        </a>
+        <a href="services-home-page.php">
+          <button class="button button2">Cancel</button>
+        </a>
+    </div>
+  </div>
