1dceb210c37b8c7e4d7d56bc7248822c4dc7ccc1
Harvin Raj <harvinraj034@gmail.com>
2022-06-10 23:48:55 +0800

    Feedback successful page

---
+++ Frontend/Services/src/services-feedback-success.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SNAPS Service Order Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+  <div class="grid-container">
+    <div class="item1">
+      <div class="top">Feedback</div>
+      <div class="bottom"><center>Feedback is successful!</center></div>
+        <a href="services-home-page.php">
+          <button class="button button3">Proceed to main page</button>
+        </a>
+    </div>
