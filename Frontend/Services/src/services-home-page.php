e75c455bd058b1d0635d7cc275c69d41e93981cf
Harvin Raj <harvinraj034@gmail.com>
2022-06-10 15:21:02 +0800

    Add orders

--- Frontend/Services/src/services-home-page.php
+++ Frontend/Services/src/services-home-page.php
@@ -14,23 +14,25 @@
 <body>
   <div class="grid-container">
     <div class="item1">
-      <img id="bukit-ekspo" class="image-fluid" src="../assets/bukit ekspo.JPG" alt="Bukit Ekspo">
+      <img id="bukit-ekspo" class="image-fluid" src="../assets/bukit ekspo.JPG" alt="Bukit Ekspo" width="1480px" height="290px">
       <div class="left">BUKIT EKSPO</div>
     </div>
     <div class="item2">
-      <img id="machineries" class="image-fluid" src="../assets/machineries1.jpg" alt="Machineries" width="1500px" height="300px">
+      <img id="machineries" class="image-fluid" src="../assets/machineries1.jpg" alt="Machineries" width="1480px" height="290px">
       <div class="left">MACHINERIES</div>
     </div>
     <div class="item3">
-      <img id="meeting" class="image-fluid" src="../assets/lectureroom.jpg" alt="Meeting" width="1500px" height="300px">
+      <img id="meeting" class="image-fluid" src="../assets/lectureroom.jpg" alt="Meeting" width="1480px" height="290px">
       <div class="left">MEETING AND LECTURE ROOMS</div>
     </div>
     <div class="item4">
-      <img id="agriculture" class="image-fluid" src="../assets/agriculturalcourses.jpg" alt="Agriculture" width="1500px" height="300px">
+      <img id="agriculture" class="image-fluid" src="../assets/agriculturalcourses.jpg" alt="Agriculture" width="1480px" height="290px">
       <div class="left">AGRICULTURAL COURSES</div>
     </div>
+    <p>
+      <a href="service-order-page.php">Your Orders </a>
+    </p>
   </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
