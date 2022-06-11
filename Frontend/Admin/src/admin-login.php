cd8db63a0d1908139f449fde62bbac6c4f71d45f
Sajjad <71213151+sajjadista@users.noreply.github.com>
2022-06-11 20:02:03 +0800

    Fixed the font issue

--- Frontend/Admin/src/admin-login.php
+++ Frontend/Admin/src/admin-login.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | SNAPS Admin Portal</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
@@ -24,7 +29,7 @@
 
         <div class="row my-3">
             <div class="col d-flex justify-content-center">
-                <h2>Admin Login</h2>
+                <h1>Admin Login</h1>
             </div>
         </div>
 
