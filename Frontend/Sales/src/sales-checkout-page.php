9b77e93891a4603ff35b0200b6f07d4eb1f4748c
elenchelian <elenchelianelen@gmail.com>
2022-06-19 23:49:38 +0800

    19/6

--- Frontend/Sales/src/sales-checkout-page.php
+++ Frontend/Sales/src/sales-checkout-page.php
@@ -19,8 +19,6 @@ if(isset($_GET["delete"])){
 	header('Refresh: 0; url=sales-checkout-page.php');
 }
 
-<<<<<<< HEAD
-=======
 if (isset($_POST['total_price'])) {
 
 	$result_select = mysqli_query($conn,$sql);
@@ -43,7 +41,6 @@ if (isset($_POST['total_price'])) {
 
 $conn->close();
 
->>>>>>> 37f614d2051b2a0600637b6fb8123d1204d7c5d5
 ?>
 
 <!DOCTYPE html>
